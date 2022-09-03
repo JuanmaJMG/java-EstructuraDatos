32b1dd07b17c600f3e120ddd2dead447e00e3324
JuanmaJMG <jmarroquing16@miumg.edu.gt>
2022-09-03 00:01:39 -0600

    Commit push, tarea Programación

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -5,6 +5,16 @@ import Nodos.NodoSimple;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        ListaSimple Lista = new ListaSimple();
+        System.out.println("Agregue tres cuatro nodos");
+        NodoSimple nuevo = new NodoSimple(1, "UNO");
+        Lista.insertar(nuevo);
+        nuevo = new NodoSimple(2, "DOS");
+        Lista.insertar(nuevo);
+        nuevo = new NodoSimple(3, "TRES");
+        Lista.insertar(nuevo);
+        nuevo = new NodoSimple(4, "CUATRO");
+        Lista.insertar(nuevo);
+        Lista.imprimir();
     }
 }
